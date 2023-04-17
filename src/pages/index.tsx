9f9cef6c33928b534e05ca941f33663e4f5f5b23
ChaitanyaAgarwal97 <chaitanyaagarwal97@gmail.com>
2023-04-17 16:56:33 +0530

    Added footer component to home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,14 @@ import Main from '@/components/Main/Main';
 import Header from '@/components/Header/Header';
 import { Fragment } from 'react';
 
+import Footer from '@/components/Footer/Footer';
+
 export default function Home() {
   return (
     <Fragment >
       <Header />
       <Main />
+      <Footer />
     </Fragment>
   )
 }
