2b1fbd74ad42e071267a16b77406393546368f87
ChaitanyaAgarwal97 <chaitanyaagarwal97@gmail.com>
2023-04-17 15:23:08 +0530

    Content component included in Main block of page

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+
 import ImageCarousel from "../ImageCarousel/ImageCarousel";
+import Content from "../Content/Content";
 
 const Main: React.FC = () => {
     return (
         <main>
             <ImageCarousel />
+            <Content />
         </main>
     );
 }
