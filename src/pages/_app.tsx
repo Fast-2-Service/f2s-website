32d964b7d956c0ef504659202e863b17d28a74f4
ChaitanyaAgarwal97 <chaitanyaagarwal97@gmail.com>
2023-04-19 22:40:47 +0530

    Header shifted from index page to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import Head from 'next/head';
 import { SSRProvider } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from '@/components/Header/Header';
 
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <Header />
       <Component {...pageProps} />
     </SSRProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Main from '@/components/Main/Main';
-import Header from '@/components/Header/Header';
 import { Fragment } from 'react';
 
 import Footer from '@/components/Footer/Footer';
@@ -7,7 +6,6 @@ import Footer from '@/components/Footer/Footer';
 export default function Home() {
   return (
     <Fragment >
-      <Header />
       <Main />
       <Footer />
     </Fragment>
